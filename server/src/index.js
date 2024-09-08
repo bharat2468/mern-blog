4de0ef6bf7a58f9d215fd09913796600fba0d1fd
bharat2468 <129318600+bharat2468@users.noreply.github.com>
2024-09-09 00:38:57 +0530

    chore: Update npm dependency to latest stable version

--- server/src/index.js
+++ server/src/index.js
@@ -20,13 +20,16 @@
 // 	}
 // })();
 
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
+// dotenv.config({
+// 	path: "./env",
+// });
+
+import 'dotenv/config';
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
-dotenv.config({
-	path: "./env",
-});
+
 
 connectDB()
 	.then(() => {
