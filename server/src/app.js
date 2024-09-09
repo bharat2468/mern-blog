be7efb18d1d57e0dc490a8b3713ba2871baf7870
bharat2468 <129318600+bharat2468@users.noreply.github.com>
2024-09-09 13:23:55 +0530

    chore: Update CORS credentials configuration

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 //? CORS config
 const corsOptions = {
 	origin: process.env.ALLOWED_ORIGINS,
-	credentials: true,
+	credentials: process.env.CREDENTIALS,
 	methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
 };
 
